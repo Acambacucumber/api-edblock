5f2d98c198d0845bd7f1c29751ad0c876b8055a3
Eugenenenene <ehalim02@gmail.com>
2023-09-09 20:55:32 -0400

    Fixed request handling

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import {getClassInfo, createClass, getClassStudents} from './controllers/class.j
 import {createPost} from './controllers/posts.js'
 import {createUser, studentAddClass, listClasses, authUser} from './controllers/user.js'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,48 +16,48 @@ app.get('/', (req, res) => {
 // Users
 app.post('/users/authUser', async (req, res) => {
   console.log("in express:")
-  console.log(req)
-  const payload = await authUser(req)
+  console.log(req.body)
+  const payload = await authUser(req.body)
   console.log(payload)
   res.json(payload)
 })
 
 app.post('/users/listClasses', async (req, res) => {
-  const payload = await listClasses(req)
+  const payload = await listClasses(req.body)
   console.log(payload)
   res.json(payload)
 })
 app.post('/users/studentAddClass', async (req, res) => {
-  const payload = await studentAddClass(req)
+  const payload = await studentAddClass(req.body)
   console.log(payload)
   res.json(payload)
 })
 app.post('/users/createUser', async (req, res) => {
-  const payload = await createUser(req)
+  const payload = await createUser(req.body)
   console.log(payload)
   res.json(payload)
 })
 
 // Classes
-app.get('/class/getClassInfo', async (req, res) => {
-  const payload = await getClassInfo(req)
+app.post('/class/getClassInfo', async (req, res) => {
+  const payload = await getClassInfo(req.body)
   console.log(payload)
   res.json(payload)
 })
-app.get('/class/getClassStudents', async (req, res) => {
-  const payload = await getClassStudents(req)
+app.post('/class/getClassStudents', async (req, res) => {
+  const payload = await getClassStudents(req.body)
   console.log(payload)
   res.json(payload)
 })
 app.post('/class/createClass', async (req, res) => {
-  const payload = await createClass(req)
+  const payload = await createClass(req.body)
   console.log(payload)
   res.json(payload)
 })
 
 //Posts
-app.get('/post/createPost', async (req, res) => {
-  const payload = await createPost(req)
+app.post('/post/createPost', async (req, res) => {
+  const payload = await createPost(req.body)
   console.log(payload)
   res.json(payload)
 })
